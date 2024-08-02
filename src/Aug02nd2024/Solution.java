f4274f4f31576b5dfa451978eb3293d995160bb1
Nguyễn Hữu Thiện <135424236+thiennh23@users.noreply.github.com>
2024-08-02 19:06:33 +0700

    Setup

---
+++ src/Aug02nd2024/Solution.java
@@ -0,0 +1,12 @@
+package Aug02nd2024;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = new int[] {0, 1, 0, 1, 1, 0, 0};
+        System.out.println(minSwap(nums));
+    }
+
+    public static int minSwap(int[] nums) {
+
+    }
+}
