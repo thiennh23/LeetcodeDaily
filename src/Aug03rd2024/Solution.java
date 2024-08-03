097e659bb972df2940d5549cb64ffb8ca28c4ed5
Nguyễn Hữu Thiện <135424236+thiennh23@users.noreply.github.com>
2024-08-03 23:03:10 +0700

    init problem

--- src/Aug03rd2024/Solution.java
+++ src/Aug03rd2024/Solution.java
@@ -4,7 +4,7 @@ public class Solution {
     public static void main(String[] args) {
     }
 
-    public static boolean canBeEqual(int[] target, int[] arr) {
+    public boolean threeConsecutiveOdds(int[] arr)
 
     }
 }
