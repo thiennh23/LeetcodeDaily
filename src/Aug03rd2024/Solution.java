9ceb45e547eab6c143ca3ae43bde214c3caeef37
Nguyễn Hữu Thiện <135424236+thiennh23@users.noreply.github.com>
2024-08-03 23:07:12 +0700

    random

--- src/Aug03rd2024/Solution.java
+++ src/Aug03rd2024/Solution.java
@@ -2,9 +2,23 @@ package Aug03rd2024;
 
 public class Solution {
     public static void main(String[] args) {
+        int[] arr = new int[] {1, 1, 1};
+        System.out.println(threeConsecutiveOdds(arr));
     }
 
-    public boolean threeConsecutiveOdds(int[] arr)
-
+    public static boolean threeConsecutiveOdds(int[] arr){
+        for (int i = 0; i <= arr.length - 3; i++) {
+            int count = 0;
+            if (arr[i]%2 != 0)
+                count++;
+            if (arr[i+1]%2!=0)
+                count++;
+            if (arr[i+2]%2!=0)
+                count++;
+            if (count == 3)
+                return true;
+            else count = 0;
+        }
+        return false;
     }
 }
