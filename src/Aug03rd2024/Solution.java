179b3430c28c09dbcb16e0d54680d4f7981771dd
Nguyễn Hữu Thiện <135424236+thiennh23@users.noreply.github.com>
2024-08-05 22:09:41 +0700

    commit

--- src/Aug03rd2024/Solution.java
+++ src/Aug03rd2024/Solution.java
@@ -1,9 +1,32 @@
+/*
 package Aug03rd2024;
 
+
+// Problem 1460. Make 2 arrays equal by reversing sub-arrays
 public class Solution {
     public static void main(String[] args) {
-        int[] arr = new int[] {1, 1, 1};
-        System.out.println(threeConsecutiveOdds(arr));
+        */
+/*int[] arr = new int[] {1, 1, 1};
+        System.out.println(threeConsecutiveOdds(arr));*//*
+
+        int[] target = new int[] {1, 2, 3, 4};
+        int[] arr = new int[] {2, 4, 1, 3};
+        System.out.println(canBeEqual(target, arr));
+    }
+
+    public static boolean canBeEqual(int[] target, int[] arr) {
+        //If only have 1 or no element, ye they're equal
+
+    }
+
+    public static int[] insertionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+            while ((j <= 0) && (arr[j] > temp)) {
+                arr[j + 1] = arr[j];
+            }
+        }
     }
 
     public static boolean threeConsecutiveOdds(int[] arr){
@@ -22,3 +45,4 @@ public class Solution {
         return false;
     }
 }
+*/
---
+++ src/Aug05th2024/Solution.java
@@ -0,0 +1,26 @@
+package Aug05th2024;
+
+// Problem 2053: Kth Distinct String in an Array
+public class Solution {
+    public static void main(String[] args) {
+        String[] testcase1 = new String[]{"d","b","c","b","c","a"};
+        int k = 2;
+        System.out.println(kthDistinct(testcase1, k));
+
+        String[] testcase2 = new String[]{"aaa","aa","a"};
+        k = 1;
+        System.out.println(kthDistinct(testcase2, k));
+
+        String[] testcase3 = new String[]{"a","b","a"};
+        k = 3;
+        System.out.println(kthDistinct(testcase3, k));
+    }
+
+    public static String kthDistinct(String[] arr, int k) {
+        String result = "";
+
+
+
+        return result;
+    }
+}
